9cc78306b86890f4508f5d0a52baf74a7e8f48c2
vandy ahmad <vandyahmad2404@gmail.com>
2023-04-03 05:37:47 +0700

    change port

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1312"))
+	e.Logger.Fatal(e.Start(":1213"))
 }
